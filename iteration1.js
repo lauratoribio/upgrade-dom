16116cea19dd03e9a942447716a83e308bd1e956
Laura Toribio <lsancheztoribio@hotmail.com>
2021-12-16 21:30:02 +0100

    Iteration#1

---
+++ iteration1.js
@@ -0,0 +1,32 @@
+
+window.onload = () => {
+
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+let clase1 = document.queryCommandValue(".showme")
+console.log(clase1)
+
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+let h1Pillado = document.querySelector("#pillado")
+console.log(h1Pillado)
+
+// 1.3 Usa querySelector para mostrar por consola todos los p
+let paragraphs = document.querySelectorAll("p")
+console.log(paragraphs)
+
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+let pokemons = document.querySelectorAll(".pokemon")
+console.log(pokemons)
+
+// 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+// data-function="testMe".
+
+let data = document.querySelectorAll("testme")
+console.log(data)
+
+// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+// data-function="testMe".
+
+let specificData = document.querySelectorAll("testme"[3])
+console.log(specificData)
+
+}
